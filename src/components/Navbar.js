109f55a402e5a748b86e5d326a48f698f7cc627d
farihaorna23 <farihaorna@gmail.com>
2021-11-06 10:49:18 -0400

    fixed an error in the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <div className="links">
-        <Link to="/login">Login</Link>
-        <Link to="/userProfile">UserName</Link>
-        <Link to="/debits">Debit</Link>
-        <Link to="/userProfile">Credit</Link>
-        <Link to="/">Home</Link>
-      </div>
+            <Link to="/login">Login</Link>
+            <Link to="/userProfile">UserName</Link>
+            <Link to="/debits">Debit</Link>
+            <Link to="/credits">Credit</Link>
+            <Link to="/">Home</Link>
+        </div>
     );
 }
 
